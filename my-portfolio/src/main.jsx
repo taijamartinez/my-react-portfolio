5693414b8ae19ea3f796eb597130f7b523f7e7fc
taijamartinez <taijasmartinez@gmail.com>
2025-02-16 12:20:15 -0500

    updated main.jsx and added an error.jsx file

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+
+import App from './App';
+import Error from './views/Error';
+import Contact from './views/Contact';
+import AboutMe from './views/AboutMe';
+import Resume from './views/Resume';
+import Portfolio from './views/Portfolio';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
