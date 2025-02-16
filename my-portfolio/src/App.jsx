81d85310c21e958138d8d9bf7f68274dbbac538e
taijamartinez <taijasmartinez@gmail.com>
2025-02-16 15:25:41 -0500

    added code in footer.jsx, added footer styling and updated app.jsx code

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -6,14 +6,14 @@ import Footer from './components/Footer';
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
-    <>
-      <Header />
-      <main className="mx-3">
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  );
+    <div className="app-container"> {/* Wrap everything in a flex container */}
+    <Header />
+    <main className="content">
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+);
 }
 
 export default App;
--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../App.css"; // Ensure CSS is imported
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs="auto">
+            <a href="https://github.com/taijamartinez" target="_blank" rel="noopener noreferrer">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+                alt="GitHub"
+                className="footer-icon"
+              />
+            </a>
+          </Col>
+          <Col xs="auto">
+            <a href="https://www.linkedin.com/in/taija-martinez-3b128a351/" target="_blank" rel="noopener noreferrer">
+              <img
+                src="https://img.icons8.com/?size=512&id=16166&format=png"
+                alt="LinkedIn"
+                className="footer-icon"
+              />
+            </a>
+            </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
+
