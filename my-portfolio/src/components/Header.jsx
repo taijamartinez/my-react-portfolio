60d6b4a6af4c9c276e183cdc817e264bafa1ad3d
taijamartinez <taijasmartinez@gmail.com>
2025-02-16 14:12:16 -0500

    added a navbar with my name, title and links. added the navlinks in the header. added styling in the app.css for the navbar

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import NavLinks from "./NavLinks";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <NavLinks />
+    </header>
+  );
 }
--- my-portfolio/src/components/NavLinks.jsx
+++ my-portfolio/src/components/NavLinks.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Container, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom"; // Import NavLink from React Router
+import "../App.css"; // Ensure CSS is imported
 
 export default function NavLinks() {
   return (
-    <div>NavLinks</div>
-  )
+    <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar">
+      <Container fluid>  {/* Use "fluid" to make it full-width */}
+        <Navbar.Brand href="/">Taija Martinez | Full-Stack Developer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <nav className="ms-auto navbar-nav">
+            <NavLink to="/" className="nav-link">About Me</NavLink>
+            <NavLink to="/portfolio" className="nav-link">Portfolio</NavLink>
+            <NavLink to="/contact" className="nav-link">Contact</NavLink>
+            <NavLink to="/resume" className="nav-link">Resume</NavLink>
+          </nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
